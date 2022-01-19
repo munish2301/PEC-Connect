e631eebdd2982ba311e17fc2fc66e01f1b8fbd75
munish2301 <munishkumar22595@gmail.com>
2022-01-19 20:19:57 +0530

    Initial Commit

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCMVgfWZp1wr8wGX7GnyUdAydcvYrabftk",
-  authDomain: "instagram-dev-6eedf.firebaseapp.com",
-  projectId: "instagram-dev-6eedf",
-  storageBucket: "instagram-dev-6eedf.appspot.com",
-  messagingSenderId: "832156613821",
-  appId: "1:832156613821:web:8b1f5baa7b7dbeaff70bd4",
-  measurementId: "G-YXQVKZSZH2",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
